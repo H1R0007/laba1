304deb4d43a46a7f90fe92d5ac16fd69b3743734
H1R0007 <msonin10@gmail.com>
2025-02-24 21:06:41 +0300

    Update utils.cpp to merge again

---
+++ src/utils.cpp
@@ -0,0 +1,30 @@
+int static ymnozh(int a, int b) {
+  int c = a * b;
+
+  return c;
+}
+
+int static vych(int a, int b) {
+  int c = a - b;
+
+  return c;
+}
+
+int static plus(int a, int b) {
+  int c = a + b;
+
+  return c;
+}
+
+int static ravenstvo(int a, int b) {
+  bool c = 0;
+  if (a == b) {
+    c = true;
+  }
+  else {
+    c = false;
+  }
+
+
+  return c;
+}
