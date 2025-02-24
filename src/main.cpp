6f1032f13ce5e61d444651334b4e2c7d078dc06e
H1R0007 <msonin10@gmail.com>
2025-02-24 21:20:41 +0300

    dop try to merge again ( +1 stroka in main/main)

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,21 @@
 #include <iostream>
 
 int main() {
-  int a = 0;
-  int b = 0;
-  std::cout << "Please, Enter a numbers to calculate: ";
-  std::cin >> a >> b;
-  int g1 = plus(a, b);
-  int g2 = ymnozh(a, b);
-  int g3 = vych(a, b);
-  bool g4 = ravenstvo(a, b);
+	std::cout << "program is alrready started!" << std::endl;
 
-  std::cout << "g1 = " << g1 << std::endl;
-  std::cout << "g2 = " << g2 << std::endl;
-  std::cout << "g3 = " << g3 << std::endl;
-  std::cout << "g4 = " << g4 << std::endl;
+	int a = 0;
+	int b = 0;
+	std::cout << "Please, Enter a numbers to calculate: ";
+	std::cin >> a >> b;
+	int g1 = plus(a, b);
+	int g2 = ymnozh(a, b);
+	int g3 = vych(a, b);
+	bool g4 = ravenstvo(a, b);
+
+	std::cout << "g1 = " << g1 << std::endl;
+	std::cout << "g2 = " << g2 << std::endl;
+	std::cout << "g3 = " << g3 << std::endl;
+	std::cout << "g4 = " << g4 << std::endl;
   
   return 0;
 }
