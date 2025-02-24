542f68b608cbb9731193619b5ebf0707a1c50dc9
H1R0007 <msonin10@gmail.com>
2025-02-24 18:49:59 +0300

    feat: Add "sravnenie" function to main

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@ int main() {
   int g1 = plus(a, b);
   int g2 = ymnozh(a, b);
   int g3 = vych(a, b);
+  bool g4 = ravenstvo(a, b);
 
   std::cout << "g1 = " << g1 << std::endl;
   std::cout << "g2 = " << g2 << std::endl;
   std::cout << "g3 = " << g3 << std::endl;
+  std::cout << "g4 = " << g4 << std::endl;
   
   return 0;
 }
