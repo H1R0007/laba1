aa8e70b7a979f120a8b9f83caa513d52aadd5a02
H1R0007 <msonin10@gmail.com>
2025-02-24 18:39:44 +0300

    Update main.cpp in featureutils

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+#include "utils.cpp"
+#include <iostream>
 
+int main() {
+  int a = 0;
+  int b = 0;
+  std::cout << "Please? Enter a numbers to calculate: ";
+  std::cin >> a >> b;
+  int g1 = plus(a, b);
+  int g2 = ymnozh(a, b);
+  int g3 = vych(a, b);
+
+  std::cout << "g1 = " << g1 << std::endl;
+  std::cout << "g2 = " << g2 << std::endl;
+  std::cout << "g3 = " << g3 << std::endl;
+  
+  return 0;
+}
