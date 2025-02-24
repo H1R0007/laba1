5038b42ae1d7218ab75782ed2dd287171aff6ba2
H1R0007 <msonin10@gmail.com>
2025-02-24 18:39:11 +0300

    Update utils.cpp in featureutils

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,17 @@
+int static ymnozh(int a, int b) {
+  int c = a * b;
 
+  return c;
+}
+
+int static vych(int a, int b) {
+  int c = a - b;
+
+  return c;
+}
+
+int static plus(int a, int b) {
+  int c = a + b;
+
+  return c;
+}
